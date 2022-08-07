e24b211aa63c292fb26d9178b615cc2b86da1542
oliverbalog <asperant8@gmail.com>
2022-08-07 12:01:46 +0200

    deploy

---
+++ precache-manifest.d828c30d7a20914cdf05974a5c148cf3.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "f177dd24102ffe4baac3",
+    "url": "/css/app.8aec059f.css"
+  },
+  {
+    "revision": "f177dd24102ffe4baac3",
+    "url": "/js/app.3c738924.js"
+  },
+  {
+    "revision": "98c9bdaa7c1279bd10f4",
+    "url": "/js/chunk-vendors.ab7db98c.js"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "b95cb5b27ec3e4a2ad2d6f2858340320",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3aa4c8243b125bf9fead7d4f9519660f",
+    "url": "/img/45203776_Index levél_2021. 08. 09.pdf"
+  },
+  {
+    "revision": "2327519f1fe1b492b8d2114bb3a2f346",
+    "url": "/img/54791258_Fedezet igazolás_2022. 07. 15.pdf"
+  },
+  {
+    "revision": "3b7f36e6e56e9fe9db96f6eb82d2f998",
+    "url": "/img/diag2.png"
+  },
+  {
+    "revision": "e8370a0e3295bf9b5fd7507a30bf059c",
+    "url": "/img/diag1.png"
+  },
+  {
+    "revision": "8537791f0c7c1dce6e8017fdbf81f04b",
+    "url": "/img/54791258_Kötvény_2022. 07. 12.pdf"
+  },
+  {
+    "revision": "8cf9e3a09fee2f323aa3b3566794ad67",
+    "url": "/img/only-logo.png"
+  },
+  {
+    "revision": "b8fb7209674b8e09f04445657abccaf2",
+    "url": "/Mail.php"
+  },
+  {
+    "revision": "77571c270daa913d400112a91e24952b",
+    "url": "/img/system_improve.avif"
+  },
+  {
+    "revision": "756a62bccbf1d3d31b9343786a2172c4",
+    "url": "/img/bg_bulb22.webp"
+  },
+  {
+    "revision": "6e65a598f923f65d5e3da1c2dca0c0da",
+    "url": "/img/electric.webp"
+  },
+  {
+    "revision": "1c75040ae026b9ab6249709bfc382e7e",
+    "url": "/img/bg_smarthome.webp"
+  },
+  {
+    "revision": "4e458209db6469ff7ec3ab638735297c",
+    "url": "/img/bg_smarthome.png"
+  },
+  {
+    "revision": "ef7b37350446f4a55320deb7d1dfcf16",
+    "url": "/img/boiler.webp"
+  },
+  {
+    "revision": "2cd9d0144b9186b0bb6036423e927142",
+    "url": "/img/system_improve.webp"
+  },
+  {
+    "revision": "2b7ea04722dc886877ee56841860e926",
+    "url": "/img/system_improve.jpeg"
+  },
+  {
+    "revision": "128857763fdae62ddedeb2ed29e79590",
+    "url": "/img/smarthome.webp"
+  },
+  {
+    "revision": "dc41f1e03d66d2b8eb40e5e11559807b",
+    "url": "/img/smarthome.jpeg"
+  },
+  {
+    "revision": "ba88022230e6265642bfd1317523458f",
+    "url": "/img/electric.jpeg"
+  }
+];
